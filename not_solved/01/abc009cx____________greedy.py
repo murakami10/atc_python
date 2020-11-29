aaed57a2f2cd57b9741eed2417a5f3344ff5b721
kyoya <m.kyoya777@gmail.com>
2020-11-29 14:44:01 +0900

    resolve-2020-11-29

--- not_solved/01/abc009cx____________greedy.py
+++ not_solved/01/abc009cx____________greedy.py
@@ -1,3 +1,4 @@
+#x
 import copy
 
 N, K = map(int, input().split())
---
+++ not_solved/02/tdck_____________________lis.py
@@ -0,0 +1,21 @@
+import bisect
+
+N = int(input())
+
+circle = [list(map(int, input().split())) for _ in range(N)]
+
+left_right = []
+for i in range(N):
+    left_right.append([circle[i][0] - circle[i][1], circle[i][0] + circle[i][1]])
+
+left_right.sort(key=lambda lr: -lr[1])
+left_right.sort(key=lambda lr: -lr[0])
+
+dp = [float('inf')] * N
+
+for i in range(N):
+    dp[bisect.bisect_left(dp, left_right[i][1])] = left_right[i][1]
+
+print(bisect.bisect_left(dp, float('inf')))
+
+# https://atcoder.jp/contests/tdpc/tasks/tdpc_target
